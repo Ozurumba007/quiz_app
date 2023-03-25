b8e2cd620a3356073c66840f629a7117902b8404
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-03-25 09:00:53 +0100

    removed the demo application code to rewrite mine \ added a new file for the questions of the quiz

---
+++ lib/question.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Question extends StatelessWidget {
+  const Question({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
