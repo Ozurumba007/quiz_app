23ce5c70dbb9227e5772b0c33d2de2227b925997
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-03-25 13:03:17 +0100

    added elevatedbutton to the main file \ added list of questions to the main file \ converted the stateless widget to stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,63 @@ import 'package:flutter/material.dart';
 import 'package:quiz_provider/question.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+
+    print(questionIndex);
+  }
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s your favorite animal?',
+    ];
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Quiz App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Question(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Quiz App'),
+        ),
+        body: Column(
+          children: [
+            Text(
+              questions[questionIndex],
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text('answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text('answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text('answer 3'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
