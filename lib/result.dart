f78992a75c8592c4cbbbee018d4105339f48caf3
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-03-28 22:10:51 +0100

    added a result file in the lib folder

---
+++ lib/result.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  const Result({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
