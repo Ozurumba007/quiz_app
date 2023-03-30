a10de58b94bf142c6829b2854ef377ab2086fc02
Ozurumba Onwuasoanya <onwuasoanyaozurumba007@gmail.com>
2023-03-30 21:49:30 +0100

    changed the name of the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_provider/result.dart';
-import './quiz.dart';
 import './result.dart';
+import './quiz.dart';
 
 void main() {
   runApp(MyApp());
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:quiz_provider/main.dart';
+import 'package:quiz/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
